50a132998f75831899d9eb9d9b59aae04aa7a32e
TL Raky <120471799+tinalalaina@users.noreply.github.com>
2026-02-23 11:09:45 +0300

    Fix duplicate OTP email on user registration

--- gasy-car-backend/notification/views.py
+++ gasy-car-backend/notification/views.py
@@ -1,7 +1,5 @@
 # django
 from django.shortcuts import get_object_or_404
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from rest_framework import viewsets, permissions, decorators, response, status
 
 # DRF
@@ -10,34 +8,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
-# methodes d'envoi de notification - utilisez la version autonome
-from gasycar.utils import create_and_send_email_otp_standalone
-
-# Models
-from users.models import User
-
 # views.py
 from notification.models import TicketNotification, Notification
 from notification.serializers import TicketNotificationSerializer, NotificationSerializer
-
-
-
-
-# post save signal to send OTP email on user creation
-@receiver(post_save, sender=User)
-def notify_user_creation(sender, instance, created, **kwargs):
-    """
-    Envoie un OTP email quand un nouvel utilisateur est créé
-    """
-    if not created:
-        return
-    
-    
-    # Envoyer OTP email à la création d'un nouvel utilisateur
-    try:
-        create_and_send_email_otp_standalone(instance)  # Utilisez la version autonome
-    except Exception as e:
-        print(f"Erreur lors de l'envoi de l'OTP: {e}")
 
 
 class NotificationViewSet(viewsets.ModelViewSet):
