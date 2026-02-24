4dd8c37ddd37601e92d560b17cb222a36a64f28b
TL Raky <120471799+tinalalaina@users.noreply.github.com>
2026-02-24 13:30:19 +0300

    Fix admin blog creation permissions for ADMIN role

--- gasy-car-backend/blogs/views.py
+++ gasy-car-backend/blogs/views.py
@@ -1,33 +1,13 @@
 from .serializers import BlogPostSerializer
 from .permissions import IsAdminOrReadOnly
 # DRF
-from rest_framework.views import APIView
-from rest_framework import viewsets, permissions
 from rest_framework import viewsets, parsers
-from rest_framework.response import Response
-from rest_framework import status
 
 # django
-from django.core.files.storage import default_storage
 
 # models
 from .models import BlogPost
-from .models import BlogPost
 
-# serialser
-from .serializers import BlogPostSerializer
-
-
-
-class IsAdminOrReadOnly(permissions.BasePermission):
-    """
-    Lecture pour tout le monde, écriture seulement pour staff/admin.
-    """
-
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return bool(request.user and request.user.is_staff)
 
 
 class BlogPostViewSet(viewsets.ModelViewSet):
@@ -41,7 +21,8 @@ class BlogPostViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         qs = BlogPost.objects.all()
 
-        if self.request.user.is_staff:
+        user = self.request.user
+        if user.is_staff or user.is_superuser or getattr(user, "role", None) == "ADMIN":
             return qs
 
         return qs.filter(is_published=True)
